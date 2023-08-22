6307b224ea36570f8b5fcb43e953c44cf9a370a6
Errytagedesign <errytage@gmail.com>
2023-08-23 00:28:54 +0100

    Fix lint errors

--- src/Redux/Features/booksSlice.js
+++ src/Redux/Features/booksSlice.js
@@ -9,11 +9,9 @@ const bookSlice = createSlice({
     addBook: (state, action) => {
       state.push(action.payload);
     },
-    removeBook: (state, action) => {
-      return state.filter((book) => book.id !== action.payload);
-    },
+    removeBook: (state, action) => state.filter((book) => book.id !== action.payload),
   },
 });
 
 export const { addBook, removeBook } = bookSlice.actions;
-export default counterSlice.reducer;
+export default bookSlice.reducer;
